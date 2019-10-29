8a4b7a802b09a37b9534dc6af362d9107de708a6
Randy Pham <randypham50@gmail.com>
2019-10-29 19:49:23 -0400

    Refactored grids and added modals to img galleries

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,38 @@ function toggleNav() {
 }
 
 navBtn.addEventListener('click', toggleNav);
+
+// ==================================================
+// IMAGE GALLERY MODAL
+// ==================================================
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('.modal');
+
+if (gallery && modal) {
+  function showModal(e) {
+    if (e.target.className === 'gallery-img') {
+      const modalImg = document.querySelector('.modal-img');
+      const modalTitle = document.querySelector('.modal-title');
+      const src = e.target.getAttribute('src');
+      const title = e.target.getAttribute('alt');
+
+      modalImg.setAttribute('src', src);
+      modalImg.setAttribute('alt', title);
+      modalTitle.textContent = title;
+
+      modal.classList.add('active');
+    }
+  }
+
+  function exitModal(e) {
+    if (
+      e.target.classList.contains('modal') ||
+      e.target.classList.contains('modal-exit')
+    ) {
+      modal.classList.remove('active');
+    }
+  }
+
+  gallery.addEventListener('click', showModal);
+  modal.addEventListener('click', exitModal);
+}
